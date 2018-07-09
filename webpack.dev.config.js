7fccfef5c18c45ef6a9c2547212e2d8f3dd0cc18
Diogo Pinto <diogop@canadahelps.org>
2018-07-09 14:20:51 -0400

    [testing and building] upgraded webpack to 4.4 - jest to 21 - updated configs - added sassrc - added es6 jshintrc file - added cache to gitignore

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,4 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var webpack = require("webpack");
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: {
     main: "./src/main/index.tsx",
@@ -10,45 +7,40 @@ module.exports = {
     filename: "js/app.[name].js",
     path: __dirname + "/webroot/dist/"
   },
+  mode: "development",
   devtool: "source-map",
-
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-
   module: {
     rules: [
-      { 
-        test: /\.tsx?$/, 
+      {
+        test: /\.tsx?$/,
         loader: "awesome-typescript-loader",
       },
       { test: /\.jsx?$/, exclude: /localforage/, loader: "babel-loader" },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            { loader: "css-loader" },   // translates CSS into CommonJS
-            { loader: "postcss-loader" },
-            {
-              loader: "sass-loader",  // compiles Sass to CSS
-              options: {
-                includePaths: ["node_modules/foundation-sites/scss"]
-              }
-            },
-          ]
-        })
+        use: [
+          { loader: MiniCssExtractPlugin.loader},
+          { loader: "css-loader" },   // translates CSS into CommonJS
+          { loader: "postcss-loader" },
+          {
+            loader: "sass-loader",  // compiles Sass to CSS
+            options: {
+              includePaths: ["node_modules/foundation-sites/scss"]
+            }
+          },
+        ]
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            { loader: "css-loader" },   // translates CSS into CommonJS
-            { loader: "postcss-loader" },
-          ]
-        })
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },   // translates CSS into CommonJS
+          { loader: "postcss-loader" },
+        ]
       },
     ]
   },
@@ -57,14 +49,8 @@ module.exports = {
     "react-dom": "ReactDOM"
   },
   plugins: [
-    new ExtractTextPlugin("css/app.[name].css"),
-    // new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(en-ca|fr-ca)$/),
-    /*new webpack.optimize.CommonsChunkPlugin({
-      name: "common",
-      filename: "js/cdn3.[name].js",
-      path: __dirname + "/../../../../dist/secure/",
-      minChunks: 2,
-    }),*/
-    // new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/app.[name].css"
+    }),
   ]
-}
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,4 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var webpack = require("webpack");
-
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: {
     main: "./src/main/index.tsx",
@@ -9,47 +7,40 @@ module.exports = {
     filename: "js/app.[name].js",
     path: __dirname + "/webroot/dist/"
   },
-
+  mode: "production",
+  devtool: "source-map",
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-
   module: {
     rules: [
-      { 
-        test: /\.tsx?$/, 
+      {
+        test: /\.tsx?$/,
         loader: "awesome-typescript-loader",
       },
       { test: /\.jsx?$/, exclude: /localforage/, loader: "babel-loader" },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            { 
-              loader: "css-loader",
-              options: { minimize: true }
-            },   // translates CSS into CommonJS
-            { loader: "postcss-loader" },
-            {
-              loader: "sass-loader",  // compiles Sass to CSS
-              options: {
-                includePaths: ["node_modules/foundation-sites/scss"]
-              }
-            },
-          ]
-        })
+        use: [
+          { loader: MiniCssExtractPlugin.loader},
+          { loader: "css-loader" },   // translates CSS into CommonJS
+          { loader: "postcss-loader" },
+          {
+            loader: "sass-loader",  // compiles Sass to CSS
+            options: {
+              includePaths: ["node_modules/foundation-sites/scss"]
+            }
+          },
+        ]
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            { loader: "css-loader" },   // translates CSS into CommonJS
-            { loader: "postcss-loader" },
-          ]
-        })
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },   // translates CSS into CommonJS
+          { loader: "postcss-loader" },
+        ]
       },
     ]
   },
@@ -58,19 +49,8 @@ module.exports = {
     "react-dom": "ReactDOM"
   },
   plugins: [
-    new ExtractTextPlugin("css/main.[name].css"),
-    // new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(en-ca|fr-ca)$/),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false
+    new MiniCssExtractPlugin({
+      filename: "css/app.[name].css"
     }),
-    new webpack.optimize.AggressiveMergingPlugin(),
   ]
-}
+};
